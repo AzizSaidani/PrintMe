21645f674f5d2467a981e97a7d65d9f2a1e4327e
AzizSaidani <Azizsaidani1999@gmail.com>
2024-05-11 15:57:44 +0100

    dashboard admin

--- server/routes/visitor.routes.js
+++ server/routes/visitor.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const contactController = require('../controllers/contact.controllers');
+const contactController = require('../controllers/contact.controller');
 
 router.post('/createReclamation', contactController.createReclamation);
 module.exports = router;
