08285c34601d0a928ef515c088b7ecbb870f9ff5
AzizSaidani <Azizsaidani1999@gmail.com>
2024-05-12 17:17:28 +0100

    add product

---
+++ src/app/back-office/service/add-product.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddProductService {
+  private apiUrl = 'http://localhost:3000/api/product';
+
+
+  constructor(private http: HttpClient) {
+  }
+
+  uploadImage(vals: any): Observable<any> {
+
+    let data = vals;
+    return this.http.post("https://api.cloudinary.com/v1_1/dwkp2dnfs/upload", data)
+  }
+
+
+  addProduct(productData:any){
+    return this.http.post(`${this.apiUrl}/add`, productData);
+  }
+}
