1869594862baa5c41bb155889444d9e463c9a374
Aziz <azizsaidani1999@gmail.com>
2025-03-06 15:52:54 +0100

    remove server folder to make it a separate project

--- server/routes/subscription.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const subscriptionController = require('../controllers/subscription.controller');
-
-router.post('/subscribe', subscriptionController.handleSubscription);
-router.get('/status', subscriptionController.getSubStatus);
-router.get('/load', subscriptionController.getAllSubscriptions);
-
-module.exports = router;
