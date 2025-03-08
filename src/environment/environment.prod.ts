0fc6e8c8f2244e659d9b446c3d88a006db4deac4
AzizSaidani <Azizsaidani1999@gmail.com>
2025-03-08 16:23:20 +0100

    changing localhost to real url

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  CLOUDINARY_UPLOAD_PRESET: 'pfe_product',
+  CLOUDINARY_CLOUD_NAME: 'dwkp2dnfs',
+  CLOUDINARY_CLOUD_UPLOAD_API: '',
+  BACKEND_URL: 'https://printme-back.onrender.com'
+};
